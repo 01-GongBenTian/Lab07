255c71c4f8e2774d2bff098d6e34cea808551f5e
01-Gong BenTian <longdexiou@gmail.com>
2022-10-25 11:23:50 +0800

    Implement additional feature

---
+++ Assets/Detector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Detector : MonoBehaviour
+{
+    GameManager gameManager;
+
+    void Start()
+    {
+        gameManager = (GameManager)FindObjectOfType(typeof(GameManager));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position = new Vector3(0, 0.1f, 0) * Mathf.Sin(Time.time);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Obstacle")
+        {
+            gameManager.UpdateScore(1);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,20 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private Rigidbody rigidBody;
+    public float force;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rigidBody.AddForce(new Vector3(0f, force, 0f), ForceMode.Impulse);
+        }
+
+        Vector3 velocity;
+        velocity = rigidBody.velocity;
+        velocity.y = Mathf.Clamp(velocity.y, -12f, 3.5f);
+        rigidBody.velocity = velocity;
+
+        if(transform.position.y <= -4.5f)
+        {
+            SceneManager.LoadScene("GameLose");
+        }
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obstacle")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+
 }
